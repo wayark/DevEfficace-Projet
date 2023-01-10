455d922862bf4346de6667b2ad4b46f32b48f4d5
liam <liam.lucas693@gmail.com>
2023-01-10 16:40:31 +0100

    project beginning

---
+++ src/Arbre.java
@@ -0,0 +1,16 @@
+public class Arbre {
+    private int valeur;
+    private Arbre fg;
+    private Arbre fd;
+
+    public Arbre(int valeur) {
+        this.valeur = valeur;
+        this.fg = null;
+        this.fd = null;
+
+    }
+
+    public void creationABR(int val[]){
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Arbre arbre = new Arbre(10);
+
+    }
+}
